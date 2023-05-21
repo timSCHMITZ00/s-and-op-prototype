e7ec4264e0d7112c18a9eacf92ae5c00faa26046
tschmit2 <tschmit2@uni-muenster.de>
2023-05-21 16:56:26 +0200

    csrf and trusted domain update

--- s_and_op_tool/settings.py
+++ s_and_op_tool/settings.py
@@ -30,8 +30,8 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DJANGO_DEBUG")
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "s-and-op-prototype-production.up.railway.app"]
-CSRF_TRUSTED_ORIGINS = ["s-and-op-prototype-production.up.railway.app"]
+ALLOWED_HOSTS = ["http://localhost", "http://127.0.0.1", "https://s-and-op-prototype-production.up.railway.app"]
+CSRF_TRUSTED_ORIGINS = ["https://s-and-op-prototype-production.up.railway.app"]
 
 
 # Application definition
